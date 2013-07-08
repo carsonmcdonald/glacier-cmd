8c9de30c18f9072da28238584ac4e328bbd1103b
Carson McDonald <carson@ioncannon.net>
2013-07-08 09:11:22 -0400

    Parse the configuration and check for required values

--- glaciercmd/cli.py
+++ glaciercmd/cli.py
@@ -1,7 +1,15 @@
-import logging
+import logging 
+import sys
+
+from glaciercmd.gcconfig import GCConfig
 
 def run():
   logging.basicConfig(format="%(asctime)s %(levelname)s %(module)s: %(message)s", level=logging.DEBUG)
+
+  config = GCConfig()
+  if config.has_errors():
+    config.log_errors()
+    sys.exit(1)
 
 if __name__ == '__main__':
   run()
