c0a6a18363e3bdaab67c4abb15add441e7a975ca
Carson McDonald <carson@ioncannon.net>
2013-07-15 09:18:21 -0400

    Save archive ids to dynamodb

--- glaciercmd/command_upload_file_to_vault.py
+++ glaciercmd/command_upload_file_to_vault.py
@@ -1,4 +1,9 @@
 import boto
+import time
+import os
+
+from boto.dynamodb2.table import Table
+from boto.dynamodb2.layer1 import DynamoDBConnection
 
 class CommandUploadFileToVault(object):
 
@@ -15,6 +20,14 @@ class CommandUploadFileToVault(object):
     else:
       archive_id = vault.upload_archive(args[2])
       print "Upload archive id: {}".format(archive_id)
+
+      dynamo_connection=DynamoDBConnection(aws_access_key_id=config.get('configuration', 'aws_key'), aws_secret_access_key=config.get('configuration', 'aws_secret'))
+      archive_id_table = Table(config.get('configuration', 'dynamodb_table'), connection=dynamo_connection)
+      archive_id_table.put_item(data={
+            'Archive ID': archive_id,
+            'Filename': os.path.abspath(args[2]),
+            'Upload Timestamp': int(time.time())
+      })
 
   def accept(self, args):
     return len(args) >= 6 and args[0] == 'upload' and args[1] == 'file' and args[3] == 'to' and args[4] == 'vault'
