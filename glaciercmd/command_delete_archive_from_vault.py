fddd44624f1c8ff6f66a2f33cafe908a5853389d
Carson McDonald <carson@ioncannon.net>
2013-07-19 19:03:36 -0400

    Clean up dynamodb table when deleting an archive

--- glaciercmd/command_delete_archive_from_vault.py
+++ glaciercmd/command_delete_archive_from_vault.py
@@ -1,5 +1,8 @@
 import boto
+
 from boto.glacier.exceptions import UnexpectedHTTPResponseError
+from boto.dynamodb2.table import Table
+from boto.dynamodb2.layer1 import DynamoDBConnection
 
 class CommandDeleteArchiveFromVault(object):
 
@@ -16,6 +19,11 @@ class CommandDeleteArchiveFromVault(object):
     else:
       try:
         vault.delete_archive(args[2])
+
+        dynamo_connection=DynamoDBConnection(aws_access_key_id=config.get('configuration', 'aws_key'), aws_secret_access_key=config.get('configuration', 'aws_secret'))
+        archive_table = Table(config.get('configuration', 'dynamodb_table'), connection=dynamo_connection)
+        archive_table.delete_item(archive_id=args[2])
+
         print "Archive deleted: '{}'".format(args[2])
       except UnexpectedHTTPResponseError as error:
         print "Archive can not be deleted:\n\t {}".format(error)
